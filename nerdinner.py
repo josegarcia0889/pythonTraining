df2c4507136afa446a92a25f3cb96b5dd6b036fd
Jose Garcia <jgarcia@growthaccelerationpartners.com>
2015-05-19 16:26:38 -0600

    nerddinner

--- nerdinner.py
+++ nerdinner.py
@@ -1,24 +1,21 @@
+__author__ = 'jgarcia'
+
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PythonSearch(unittest.TestCase):
+class nerdDinner(unittest.TestCase):
 
 	def setUp(self):
 		self.driver = webdriver.Firefox()
 
-	
+
 	def test_search_in_python_org(self):
 		driver = self.driver
-		driver.get("http://www.python.org")
-		self.assertIn("Python", driver.title)
-		elem = driver.find_element_by_name("q")
-		elem.send_keys("pycon")
-		elem.send_keys(Keys.RETURN)
+		driver.get("http://www.nerddinner.com/")
+		self.assertIn("Nerd Dinner", driver.title)
 		assert "No results found." not in driver.page_source
 
 
 	def tearDown(self):
 		self.driver.close()
-
-

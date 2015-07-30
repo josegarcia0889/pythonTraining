8e6966848bd456d62e0b5f511d66f496da1892fd
Jose Garcia <jgarcia@growthaccelerationpartners.com>
2015-07-30 09:14:14 -0600

    removing xpaths, including css selectors

--- pages/commons/loginBasePage.py
+++ pages/commons/loginBasePage.py
@@ -1,21 +1,20 @@
 __author__ = 'jgarcia'
 
 
-
 class LogInBasePage(object):
     def __init__(self):
         self.driver = object
 
     def open_logOn_Screen(self):
-
         assert self.driver.find_element_by_id("logindisplay").is_displayed()
 
-        elem = self.driver.find_element_by_xpath(".//*[@id='logindisplay']/a")
+        elem = self.driver.find_element_by_css_selector("div[id='logindisplay']>a")
+
+        # elem = self.driver.find_element_by_xpath(".//*[@id='logindisplay']/a")
 
         elem.click()
 
         assert self.driver.find_element_by_id("login-account").is_displayed()
-
 
 
     def click_logOn_button(self):
@@ -36,7 +35,7 @@ class LogInBasePage(object):
         assert errorText[1].text == ("The Password: field is required.")
 
 
-    def write_username_password(self,user,passw):
+    def write_username_password(self, user, passw):
         self.driver.find_element_by_id("UserName").is_displayed()
         self.driver.find_element_by_id("Password").is_displayed()
 
@@ -46,13 +45,15 @@ class LogInBasePage(object):
 
     def validate_wrong_username_password(self):
         assert self.driver.find_element_by_class_name("validation-summary-errors").is_displayed()
-        assert self.driver.find_element_by_class_name("validation-summary-errors").text == ("Login was unsuccessful. Please correct the errors and try again.\nThe user name or password provided is incorrect.")
-        assert self.driver.find_element_by_class_name("field-validation-error").text == ("The username or password provided is incorrect.")
+        assert self.driver.find_element_by_class_name("validation-summary-errors").text == (
+            "Login was unsuccessful. Please correct the errors and try again.\nThe user name or password provided is incorrect.")
+        assert self.driver.find_element_by_class_name("field-validation-error").text == (
+            "The username or password provided is incorrect.")
 
     def open_register_screen(self):
-        assert self.driver.find_element_by_xpath(".//*[@id='login-account']/form/div/p[5]/a").is_displayed()
+        assert self.driver.find_element_by_css_selector("div[id=login-account]>form>div>p>a").is_displayed()
 
-        self.driver.find_element_by_xpath(".//*[@id='login-account']/form/div/p[5]/a").click()
+        self.driver.find_element_by_css_selector("div[id=login-account]>form>div>p>a").click()
 
         assert self.driver.title == ("Register for an account")
 
@@ -61,17 +62,16 @@ class LogInBasePage(object):
         assert self.driver.find_element_by_id("Password").is_displayed()
         assert self.driver.find_element_by_id("ConfirmPassword").is_displayed()
 
-    def registerForm_fillIn(self,username,email,password):
-
+    def registerForm_fillIn(self, username, email, password):
         self.driver.find_element_by_id("UserName").send_keys(username)
         self.driver.find_element_by_id("Email").send_keys(email)
         self.driver.find_element_by_id("Password").send_keys(password)
         self.driver.find_element_by_id("ConfirmPassword").send_keys(password)
 
     def click_registerButton(self):
-        assert self.driver.find_element_by_xpath(".//*[@id='main']/form/div/fieldset/p[5]/input").is_displayed()
-        self.driver.find_element_by_xpath(".//*[@id='main']/form/div/fieldset/p[5]/input").click()
+        assert self.driver.find_element_by_css_selector("div[id='main']>form>div>fieldset>p>input[type='submit']").is_displayed()
+        self.driver.find_element_by_css_selector("div[id='main']>form>div>fieldset>p>input[type='submit']").click()
 
-    def verify_logged_user(self,username):
+    def verify_logged_user(self, username):
         assert self.driver.find_element_by_id("logindisplay").is_displayed()
-        assert self.driver.find_element_by_id("logindisplay").text == ("Welcome " + username +"! [ Log Off ]")
+        assert self.driver.find_element_by_id("logindisplay").text == ("Welcome " + username + "! [ Log Off ]")

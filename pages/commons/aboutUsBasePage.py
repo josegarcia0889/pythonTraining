be55f6cf52f12ab4ab6a7ad2db18f4f5912728e2
Jose Garcia <jgarcia@growthaccelerationpartners.com>
2015-08-16 11:22:36 -0600

    changes on About Us tab

--- pages/commons/aboutUsBasePage.py
+++ pages/commons/aboutUsBasePage.py
@@ -8,14 +8,19 @@ class AboutUsBasePage(object):
     def load_driver(self, driver2):  # This method works when user calls methods in this class from an external source
         self.driver = driver2
 
+    # def click_aboutUs_tab(self):
+    #      assert self.driver.find_element_by_css_selector("a[href='/Home/About']").is_displayed()
+    #     elem = self.driver.find_element_by_css_selector("a[href='/Home/About']")
+    #    elem.click()
+
     def click_aboutUs_tab(self):
-        assert self.driver.find_element_by_css_selector("a[href='/Home/About']").is_displayed()
+        assert self.driver.find_element_by_id("menu").is_displayed()
 
-        elem = self.driver.find_element_by_css_selector("a[href='/Home/About']")
+        elem_menu = self.driver.find_element_by_id("menu")
 
-        elem.click()
+        elem_menu.find_element_by_css_selector("[href='/Home/About']").click()
 
-
+    #  img[src^='/Content/Img/icon']
     def verify_aboutUs_screenDisplayed(self):
         assert "No results found." not in self.driver.page_source
 
